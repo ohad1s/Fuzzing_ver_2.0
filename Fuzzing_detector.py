b8d9cea79187c9dc3138aeeaa2641493586d9543
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-05-12 18:25:57 +0300

    date comp

--- Fuzzing_detector.py
+++ Fuzzing_detector.py
@@ -1,20 +1,20 @@
 import datetime
 
+
 def Fuzzing_detector():
-    while(True):
+    while (True):
         month = datetime.datetime.now().strftime("%B")
         day = datetime.datetime.now().day
-        detector_counter=0
-        errors = ["Bad protocol version","kex_exchange_identification","kex_input_kexinit","Connection closed by"]
-        log_file= open("/var/log/auth.log", "r")
+        detector_counter = 0
+        errors = ["Bad protocol version", "kex_exchange_identification", "kex_input_kexinit", "Connection closed by"]
+        log_file = open("/var/log/auth.log", "r")
         for line in log_file:
-            # if datetime.datetime.now().month
-            for error in errors:
-                if error in line:
-                    detector_counter+=1
-                    print(line)
+            line_list = str(line).split(" ")
+            if line_list[0] == month and line_list[1] == day:
+                for error in errors:
+                    if error in line:
+                        detector_counter += 1
+                        print(line)
 
-        if detector_counter >=4 :
+        if detector_counter >= 4:
             print("Fuzzing detect!!!")
-
-
