d81cf6c2f67c44b27fa54b9fa8ecf5d4492beaf2
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-05-12 19:12:53 +0300

    linux- finished

--- Fuzzing_detector.py
+++ Fuzzing_detector.py
@@ -1,13 +1,17 @@
 import datetime
-
-
 def Fuzzing_detector():
+    """
+    This tool is a Fuzzing detector.
+    The tool checks the auth.log file of the system,
+    it checking for incorrect use of the ssh protocol
+    or failed entry attempt multiple times
+    """
     while (True):
         month = datetime.datetime.now().strftime("%B")
         day = str(datetime.datetime.now().day)
         detector_counter = 0
         errors = ["Bad protocol version", "kex_exchange_identification", "kex_input_kexinit", "Connection closed by"]
-        log_file = open("C:\\Users\\shira\\Desktop\\var\log\\auth.txt", "r")
+        log_file = open("/var/log/auth.log", "r")
         for line in log_file:
             line_list = str(line).split(" ")
             if line_list[0] == month and line_list[1] == day:
@@ -15,12 +19,10 @@ def Fuzzing_detector():
                     if error in line:
                         detector_counter += 1
                         print(line)
-                        if detector_counter>=4:
+                        if detector_counter>=6:
                             print("Fuzzing detect!!!")
                             exit(1)
-        if detector_counter >= 4:
-            print("Fuzzing detect!!!")
-            exit(1)
+
 
 
 
