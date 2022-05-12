e94c41722aa315fe0422e32104f55d3812ef4212
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-05-12 18:51:33 +0300

    windows check

--- Fuzzing_detector.py
+++ Fuzzing_detector.py
@@ -4,10 +4,10 @@ import datetime
 def Fuzzing_detector():
     while (True):
         month = datetime.datetime.now().strftime("%B")
-        day = datetime.datetime.now().day
+        day = str(datetime.datetime.now().day)
         detector_counter = 0
         errors = ["Bad protocol version", "kex_exchange_identification", "kex_input_kexinit", "Connection closed by"]
-        log_file = open("/var/log/auth.log", "r")
+        log_file = open("C:\\Users\\shira\\Desktop\\var\log\\auth.txt", "r")
         for line in log_file:
             line_list = str(line).split(" ")
             if line_list[0] == month and line_list[1] == day:
@@ -15,9 +15,13 @@ def Fuzzing_detector():
                     if error in line:
                         detector_counter += 1
                         print(line)
-
+                        if detector_counter>=4:
+                            print("Fuzzing detect!!!")
+                            exit(1)
         if detector_counter >= 4:
             print("Fuzzing detect!!!")
+            exit(1)
+
 
 
 if __name__ == "__main__":
